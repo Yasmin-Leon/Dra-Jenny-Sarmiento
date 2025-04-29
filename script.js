c13f49cf09615d6dd35fd528fb2b9c244bcb164f
Yasmin-Leon <yleon82@uan.edu.co>
2025-04-29 09:55:45 -0500

    Add files via upload
    Signed-off-by: Yasmin-Leon <yleon82@uan.edu.co>

---
+++ script.js
@@ -0,0 +1,10 @@
+let currentIndex = 0;
+document.querySelector('.prev-button').addEventListener('click', ()=>
+{navigate (-1);
+});
+document.querySelector('.next-button').addEventListener('click', ()=>
+    {navigate (1);
+    });
+function navigate(direction) {
+    const galleryContainer = document.querySelector ()
+}
